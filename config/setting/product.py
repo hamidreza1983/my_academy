0d1e0b9ab0dbaa50fee2826ccb928d78301a3d91
hamidreza1983 <126154017+hamidreza1983@users.noreply.github.com>
2023-04-22 05:15:32 +0200

    edited database
    database for production edited

--- config/setting/product.py
+++ config/setting/product.py
@@ -12,18 +12,35 @@ ALLOWED_HOSTS = ['www.learningpy.ir', 'learningpy.ir']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'learnin2_learningpy',
+        'USER': 'learnin2_hamid',
+        'PASSWORD': 'H@midreza62',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR/'/static'
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR/'media/'
+STATIC_ROOT = '/home/learnin2/public_html/static'
+MEDIA_ROOT = '/home/learnin2/public_html/media'
+
 STATICFILES_DIRS = [
     BASE_DIR/'static',
 ]
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'Strict'
+SESSION_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_SSL_REDIRECT = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_HSTS_SECONDS = 15768000  # set low, but when site is ready for deployment, set to at least 15768000 (6 months)
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+CSP_DEFAULT_SRC = ("'none'",)
+CSP_STYLE_SRC = ("'self'",)
+CSP_SCRIPT_SRC = ("'self'",)
+CSP_FONT_SRC = ("'self'",)
+CSP_IMG_SRC = ("'self'",)
